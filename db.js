c67dad63630b2bf7d7f4605de0938b9d5443dd37
veejaymejia01 <veejaymejia01@gmail.com>
2026-04-22 11:42:28 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error('DATABASE_URL is not set');
-}
-
 const pool = new Pool({
-  connectionString,
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+module.exports = pool;
