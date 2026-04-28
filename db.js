070c92f7a6f4d4115ad63ea8e33760a540e13c1c
veejaymejia01 <veejaymejia01@gmail.com>
2026-04-28 15:57:16 +0800

    Add files via upload

--- db.js
+++ db.js
@@ -1,16 +1,4 @@
 const { Pool } = require("pg");
-
 const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not set");
-}
-
-const pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-module.exports = pool;
+if (!connectionString) throw new Error("DATABASE_URL is not set");
+module.exports = new Pool({ connectionString, ssl: { rejectUnauthorized: false } });
