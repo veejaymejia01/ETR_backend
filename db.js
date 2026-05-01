05cd35caa54dee11382dae0b8d1d9f40fbf01536
veejaymejia01 <veejaymejia01@gmail.com>
2026-05-01 10:45:41 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-const connectionString = process.env.postgresql://healthcare_5vp2_user:Ql9hAvkuKls918KZP5ktW0vkTvHykyqX@dpg-d7f5q8q8qa3s73fnrh6g-a/healthcare_5vp2;
+const connectionString = process.env.DATABASE_URL;
 if (!connectionString) throw new Error("DATABASE_URL is not set");
 module.exports = new Pool({
   connectionString,
