c2cafa3a7ca25846af557e2a920bb3d874b2683d
veejaymejia01 <veejaymejia01@gmail.com>
2026-05-02 23:15:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 async function sendEmail(to, subject, message) {
